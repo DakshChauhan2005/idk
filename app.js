7f6f205655bd0325976de4dfab75078b9c69106a
DakshChauhan2005 <dakahchauhan111@gmail.com>
2024-11-08 11:18:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// let url = "https://www.googleapis.com/books/v1/volumes?q=tittle=Critical+Mass";
+// async function getInfo(){
+//     let book = await axios.get(url);
+//     console.log(book.data.items);
+// }
+// let ikigai = getInfo();
